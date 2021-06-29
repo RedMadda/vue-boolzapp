8b311ed1983ebd1a68d3e78882f38da1eca3c41e
RedMadda <maddalenarossaro91@gmail.com>
2021-06-29 09:26:10 +0200

    layout no hover

--- js/script.js
+++ js/script.js
@@ -1,5 +1,51 @@
 const boolzApp = new Vue({
-    el: "#gen-container",
-    data: {},
+    el: "#vue-container",
+    data: {
+        contacts: [
+            {
+                name: "Beatrice Rovazzi", 
+                photo: "../img/bea.webp"
+            },
+
+            {
+                name: "Francesco Spiga", 
+                photo: "../img/fra.webp"
+            },
+
+            {
+                name: "Lodovica De Sanctis", 
+                photo: "../img/lodo.webp"
+            },
+
+            {
+                name: "Pippo Sciatore", 
+                photo: "../img/pippo.webp"
+            },
+
+            {
+                name: "Fiammetta Bruciata", 
+                photo: "../img/fiam.jpg"
+            },
+            {
+                name: "Jack Daniels", 
+                photo: "../img/jack.webp"
+            },
+            {
+                name: "Laura Godwill", 
+                photo: "../img/laura.webp",
+            },
+
+            {
+                name: "Anoop Sharma", 
+                photo: "../img/anoop.jpg",
+            },
+
+            {
+                name: "Isotta Letragedie", 
+                photo: "../img/isotta.webp"
+            },
+         
+        ]
+    },
     methods: {}
 });
