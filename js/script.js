5da30558652f3482f99b652a17cfceb531f566a4
RedMadda <maddalenarossaro91@gmail.com>
2021-06-30 12:28:27 +0200

    milestone2

--- js/script.js
+++ js/script.js
@@ -1,51 +1,59 @@
 const boolzApp = new Vue({
     el: "#vue-container",
-    data: {
+    data:{
+        contactVisible: 0,
         contacts: [
             {
-                name: "Beatrice Rovazzi", 
-                photo: "../img/bea.webp"
+                name: "Beatrice Rovazzi",
+                photo: "https://cdn.pixabay.com/photo/2016/11/22/06/05/girl-1848454__340.jpg",
+                
+                messages: [
+                    {
+                        date: "29/06/2021",
+                        text: "Ciao! Domani andiamo da Capuano's, verresti?",
+                        status: "received"
+                    },
+                    {
+                    date: "29/06/2021",
+                     text: "Volentieri! C'è posto all'aperto?",
+                     status: "sent"
+                    },
+                    {   date: "29/06/2021",
+                        text: "Sì",
+                        status: "received"
+                    }
+                ]
             },
+            {
+                name: "zio tobia",
+                photo: "https://cdn.pixabay.com/photo/2017/04/01/21/06/portrait-2194457__340.jpg",
+              
+                messages: [
+                    {
+                        date: "00000",
+                        text: "Ciao! Domani andiamo da Capuano's, verresti?",
+                        status: "received"
+                    },
+                    {
+                    date: "00000",
+                     text: "Volentieri! C'è postall'aperto?",
+                     status: "sent"
+                    },
+                    {   date: "20000001",
+                        text: "Sì",
+                        status: "received"
+                    }
+                ]
+            }
 
-            {
-                name: "Francesco Spiga", 
-                photo: "../img/fra.webp"
-            },
+        ]   
 
-            {
-                name: "Lodovica De Sanctis", 
-                photo: "../img/lodo.webp"
-            },
-
-            {
-                name: "Pippo Sciatore", 
-                photo: "../img/pippo.webp"
-            },
-
-            {
-                name: "Fiammetta Bruciata", 
-                photo: "../img/fiam.jpg"
-            },
-            {
-                name: "Jack Daniels", 
-                photo: "../img/jack.webp"
-            },
-            {
-                name: "Laura Godwill", 
-                photo: "../img/laura.webp",
-            },
-
-            {
-                name: "Anoop Sharma", 
-                photo: "../img/anoop.jpg",
-            },
-
-            {
-                name: "Isotta Letragedie", 
-                photo: "../img/isotta.webp"
-            },
-         
-        ]
     },
-    methods: {}
-});
+    methods: {
+        activingContact(index){
+            this.contactVisible = index;
+            console.log(this.contactVisible);
+        }
+        
+    }
+})
